ed905c3cae66d5399c80591fb5e07d8fd0bbe04f
NESTEAMER <nestriker173@gmail.com>
2026-04-25 16:46:59 +0700

    Implement navigation components and basic fragments
    - Create `LoginFragment`, `DashboardFragment`, and `CreateHabitFragment` with their respective layouts.
    - Define navigation graph in `habit_navigation.xml` with transitions between login, dashboard, and habit creation.
    - Update `MainActivity` to use View Binding and host the `NavHostFragment`.
    - Configure `NavController` and `NavigationUI` in `MainActivity` to manage the action bar and fragment navigation.
    - Add basic string resources and update `activity_main.xml` layout.

---
+++ app/src/main/java/com/ubaya/habittrackerleonida/view/CreateHabitFragment.kt
@@ -0,0 +1,28 @@
+package com.ubaya.habittrackerleonida.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ubaya.habittrackerleonida.R
+
+class CreateHabitFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_habit, container, false)
+    }
+
+    companion object {
+
+    }
+}
