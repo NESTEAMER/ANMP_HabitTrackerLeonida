9e0f99e793891748334ed5e7a7c1365412226303
Catherine <s160822013@student.ubaya.ac.id>
2026-05-06 20:15:01 +0700

    “Fix conflict and update layout”

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,6 @@ buildscript {
 }
 
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
